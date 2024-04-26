86bcf2c6d1263ec9f2753e1db1a539f6590992f3
Rodrigo Bossini <professorbossini@gmail.com>
2024-04-26 19:59:15 -0300

    refactor(ap07): encerra a secao 2.6
    - implementa o componente App por meio de uma classe
    - transforma o componente Busca num componente controlado
    - faz o componente Busca chamar uma funcao recebida via props que lhe foi entregue pelo seu pai(App)

--- busca-foto/src/components/App.js
+++ busca-foto/src/components/App.js
@@ -1,21 +1,31 @@
 import React from 'react'
 import Busca from './Busca'
 
-const App = () => {
-  return (
-    <div
-      className='grid justify-content-center border-round border-1 border-400'>
-        <div className="col-12">
-          <h1>Exibir uma lista de...</h1>
-        </div>
 
-        <div className="col-12">
-            <Busca 
-              classNameInputText="col-12 md:col-6"
-              classNameButton="col-12 md:col-6"/>
-        </div>
-    </div>
-  )
+export default class App extends React.Component{
+// const App = () => {
+
+  onBuscaRealizada = (termo) => {
+
+  }
+
+  render(){
+    return (
+      <div
+        className='grid justify-content-center border-round border-1 border-400'>
+          <div className="col-12">
+            <h1>Exibir uma lista de...</h1>
+          </div>
+  
+          <div className="col-12">
+              <Busca
+                onBuscaRealizada={this.onBuscaRealizada}
+                classNameInputText="col-12 md:col-6"
+                classNameButton="col-12 md:col-6"/>
+          </div>
+      </div>
+    )
+  }
 }
 
-export default App
+// export default App
--- busca-foto/src/components/Busca.js
+++ busca-foto/src/components/Busca.js
@@ -4,25 +4,42 @@ import { InputIcon } from 'primereact/inputicon'
 import { InputText } from 'primereact/inputtext'
 import { Button } from 'primereact/button'
 export default class Busca extends Component {
+
+  state = {
+    termoDeBusca: ''
+  }
+
+  onTermoAlterado = (event) => {
+    // console.log(event.target.value)
+    this.setState({termoDeBusca: event.target.value})
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault()
+  }
   render() {
     return (
-      <div className='grid'>
-        <div
-          className={`${this.props.classNameInputText}`}>
-          <IconField 
-            iconPosition='left'>
-            <InputIcon 
-              className="pi pi-search"/>
-            <InputText
-              className='w-full h-100' 
-              placeholder={this.props.dica}
-              />
-          </IconField>
+      <form onSubmit={this.onFormSubmit}>
+        <div className='grid'>
+          <div
+            className={`${this.props.classNameInputText}`}>
+            <IconField 
+              iconPosition='left'>
+              <InputIcon 
+                className="pi pi-search"/>
+              <InputText
+                value={this.state.termoDeBusca}
+                className='w-full h-100' 
+                placeholder={this.props.dica}
+                onChange={this.onTermoAlterado}
+                />
+            </IconField>
+          </div>
+          <Button 
+            label="Buscar"
+            className={`${this.props.classNameButton}`}/>
         </div>
-        <Button 
-          label="Buscar"
-          className={`${this.props.classNameButton}`}/>
-      </div>
+      </form>
     )
   }
 }
